21934e540aae5fb209624333502170d8e8fe4c9e
gabrielamadalska <gabriela.madalska@gmail.com>
2021-10-30 22:51:18 +0200

    Create a HTML template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+#funkcja post_list ktora pobiera i zwraca wartość wowalaną dzieki innej
+#funkcji - render, ktora zlozy w calosc szablon html
+
