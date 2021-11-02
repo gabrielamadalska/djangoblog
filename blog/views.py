83e10c971a459e01026ad2be32a1d4a4db3029b1
gabrielamadalska <gabriela.madalska@gmail.com>
2021-11-02 17:44:56 +0100

    Changes in HTML file

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+#kropka ocznacza bieżący katalog/aplikacje
 
 #funkcja post_list ktora pobiera i zwraca wartość wowalaną dzieki innej
 #funkcji - render, ktora zlozy w calosc szablon html
+def post_list(request):
+    posts = Post.objects.filter(publish_date__lte = timezone.now()).order_by('publish_date')
+
+    return render(request, 'blog/post_list.html', {'posts' : posts})
+
+
 
