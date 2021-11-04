d06a688962f5bf9c7ee822613380faf195b94df6
gabrielamadalska <gabriela.madalska@gmail.com>
2021-11-04 19:55:12 +0100

    New html template and fix misteakes.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -8,8 +8,11 @@ from .models import Post
 #funkcji - render, ktora zlozy w calosc szablon html
 def post_list(request):
     posts = Post.objects.filter(publish_date__lte = timezone.now()).order_by('publish_date')
-
     return render(request, 'blog/post_list.html', {'posts' : posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
 
 
 
